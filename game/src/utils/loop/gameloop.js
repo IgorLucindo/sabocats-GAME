9e0269c683e73f9fc252650037b29ff6b27beb4c
IgorLucindo <lucindoigor@yahoo.com.br>
2025-07-21 17:39:39 -0500

    1

--- game/src/utils/loop/gameloop.js
+++ game/src/utils/loop/gameloop.js
@@ -1,21 +1,21 @@
 // game loop
 function gameLoop(){
-    // set the delta time
-    currentTime = performance.now();
-    deltaTime = (currentTime - previousTime)/1000;
-    previousTime = currentTime;
-    // update accumulator time
-    accumulatorTime += deltaTime;
+    // // set the delta time
+    // currentTime = performance.now();
+    // deltaTime = (currentTime - previousTime)/1000;
+    // previousTime = currentTime;
+    // // update accumulator time
+    // accumulatorTime += deltaTime;
 
-    // run logic loop at a fixed tickRate
-    while (accumulatorTime >= properties.tickTime){
-        logicLoop();
-        accumulatorTime -= properties.tickTime;
-    };
+    // // run logic loop at a fixed tickRate
+    // while (accumulatorTime >= properties.tickTime){
+    //     logicLoop();
+    //     accumulatorTime -= properties.tickTime;
+    // };
 
-    // Interpolation factor for rendering
-    const interpolation = accumulatorTime / properties.tickTime;
-    renderLoop();
+    // // Interpolation factor for rendering
+    // const interpolation = accumulatorTime / properties.tickTime;
+    // renderLoop();
 
-    requestAnimationFrame(gameLoop);
+    // requestAnimationFrame(gameLoop);
 };
