6f38e074c7ed4d9742741e2cd0b75b1d1e062034
IgorLucindo <lucindoigor@yahoo.com.br>
2025-07-21 17:33:44 -0500

    updated directories

--- game/src/utils/gameInputs.js
+++ game/src/utils/gameInputs.js
@@ -1,5 +1,5 @@
-// set keyboard events
-function getKeyboardEvents(){
+// Set keyboard events
+function getKeyboardEvents() {
     // key down
     window.addEventListener("keydown", (event) => {
         let key = event.key.toLowerCase();
@@ -26,60 +26,63 @@ function getKeyboardEvents(){
 };
 
 
-
-// mouse events
-function mouseEventsUpdate(){
-    // mouse down events
-    if(mouse.down){
-        if(mouse.event.button == 0){mouse.mouse1.pressed = true;}
-        else if(mouse.event.button == 2){
+// Mouse events
+function mouseEventsUpdate() {
+    if(mouse.down) {
+        // mouse down events
+        if(mouse.event.button == 0) {mouse.mouse1.pressed = true;}
+        else if(mouse.event.button == 2) {
             mouse.mouse2.pressed = true;
             mouse.canvasPosition.x = mouse.event.x / camera.zoom - camera.position.x;
             mouse.canvasPosition.y = mouse.event.y / camera.zoom - camera.position.y;
-            if(grid){
+            if(grid) {
                 mouse.gridPosition.x = Math.floor((mouse.canvasPosition.x - grid.position.x)/properties.tileSize);
                 mouse.gridPosition.y = Math.floor((mouse.canvasPosition.y - grid.position.y)/properties.tileSize);
             }
         }
     }
-    else if(mouse.event){
-        if(mouse.event.button == 0){mouse.mouse1.pressed = false;}
-        else if(mouse.event.button == 2){mouse.mouse2.pressed = false;}
+    else if(mouse.event) {
+        if(mouse.event.button == 0) {mouse.mouse1.pressed = false;}
+        else if(mouse.event.button == 2) {mouse.mouse2.pressed = false;}
     }
-    // mouse move events
-    if(mouse.move || camera.move.x || camera.move.y){
+    if(mouse.move || camera.move.x || camera.move.y) {
+        // Mouse move events
         mouse.canvasPosition.x = mouse.event.x / camera.zoom - camera.position.x;
         mouse.canvasPosition.y = mouse.event.y / camera.zoom - camera.position.y;
-        if(grid){
+        if(grid) {
             mouse.gridPosition.x = Math.floor((mouse.canvasPosition.x - grid.position.x)/properties.tileSize);
             mouse.gridPosition.y = Math.floor((mouse.canvasPosition.y - grid.position.y)/properties.tileSize);
         }
     }
     mouse.move = false;
-};
-// mouse move function
-function mouseMove(event){
+}
+
+// Mouse move function
+function mouseMove(event) {
     mouse.move = true;
     mouse.event = event;
-};
-// set mouse events
-function getMouseEvents(){
+}
+
+// Set mouse events
+function getMouseEvents() {
     window.addEventListener("mousemove", mouseMove);
-    window.addEventListener("mousedown", (event) => {
+    window.addEventListener("mousedown", (e) => {
         mouse.down = true;
-        mouse.event = event;
+        mouse.event = e;
     });
-    window.addEventListener("mouseup", (event) => {
+    window.addEventListener("mouseup", (e) => {
         mouse.down = false;
-        mouse.event = event;
+        mouse.event = e;
     });
-    window.addEventListener("contextmenu", (event) => {event.preventDefault()});
+    window.addEventListener("contextmenu", (e) => {e.preventDefault()});
 };
-// reset mouse events
-function resetMouseEvents(){
+
+// Reset mouse events
+function resetMouseEvents() {
     window.addEventListener("mousemove", mouseMove);
 };
-// remove mouse events
-function removeMouseEvents(){
+
+// Remove mouse events
+function removeMouseEvents() {
     window.removeEventListener("mousemove", mouseMove);
 };
